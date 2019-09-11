e181c929ec766563f8bcfb55e469a85bfd920ce7
Timothy Miceli <tim@miceli.me>
2019-09-10 19:42:58 -0600

    Initial Commit

---
+++ CS517 HW2/CS517 HW2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CS517 HW2
+//
+//  Created by Timothy Miceli on 9/10/19.
+//  Copyright © 2019 Timothy Miceli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CS517 HW2/CS517 HW2Tests/CS517_HW2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CS517_HW2Tests.swift
+//  CS517 HW2Tests
+//
+//  Created by Timothy Miceli on 9/10/19.
+//  Copyright © 2019 Timothy Miceli. All rights reserved.
+//
+
+import XCTest
+@testable import CS517_HW2
+
+class CS517_HW2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
