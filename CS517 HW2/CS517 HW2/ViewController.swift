71b7d49cc69d8ef04ffc91462796f2fe7778bfd8
Timothy Miceli <tim@miceli.me>
2019-09-11 23:50:29 -0600

    Hopefully This is the correct way to commit the HW2 project

--- CS517 HW2/CS517 HW2/ViewController.swift
+++ CS517 HW2/CS517 HW2/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Label1: UITextField!
+    @IBOutlet weak var scrambledText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //print(Label1.text!)
+        
+        
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func scrambleButton(_ sender: Any) {
+        print("scramble button presses")
+        
+        var scrambledString = "";
+        var stringLength = 0
+        stringLength = Label1.text!.count
+        
+        //how to get a scrambled sequence from https://learnappmaking.com/random-numbers-swift/
+        let sequence = 0 ..< stringLength
+        let scrambledSequence = sequence.shuffled()
+        for position in scrambledSequence {
+            
+            var index = Label1.text!.index(Label1.text!.startIndex, offsetBy: position)
+            scrambledString += String(Label1.text![index])
+        }
+        scrambledText.text = scrambledString
+    }
+    
 }
 
